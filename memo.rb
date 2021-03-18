56e2f9011315b96a442c93f70430d6f84b9943bd
EC2 Default User <ec2-user@ip-172-31-33-227.ap-northeast-1.compute.internal>
2021-03-18 14:04:35 +0000

    f

---
+++ memo.rb
@@ -0,0 +1,27 @@
+require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+Number = gets.to_i
+
+if Number == 1
+  puts "拡張子を除いたファイル名を入力してください"
+  csvfile = STDIN.gets
+
+ puts "メモを入力"
+ content = gets
+ puts "入力後、Ctrl + D で保存"
+  
+CSV.open("#{csvfile}.csv","a") do |csv|
+      csv << ["#{content}"]
+    end
+ 
+elsif Number ==2
+  puts "編集したいファイル名を入力してください"
+   csvfile_n = STDIN.gets
+    puts "編集したい内容を入力してください"
+    puts "入力後、Ctrl + D で保存"
+    content_n = gets
+   CSV.open("#{csvfile_n}.csv","a") do |csv|
+      csv << ["#{content_n}"]
+    end
